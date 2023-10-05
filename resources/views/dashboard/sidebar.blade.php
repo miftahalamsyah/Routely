e8f450c90922a4678be196fac65b36ff0d6ee6ba
miftahalamsyah <miftahrizkyalamsyah@gmail.com>
2023-10-05 18:16:37 +0700

    sidebar minor fix

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -1,8 +1,8 @@
 <!-- Sidebar Toggle -->
-<div id="sidebar-toggle" class="sticky top-0 bg-stone-800 inset-x-0 z-10 border-b border-stone-700 px-4 sm:px-6 md:px-8 lg:hidden">
+<div class="sticky top-0 bg-stone-800 inset-x-0 z-10 border-b border-stone-700 px-4 sm:px-6 md:px-8 lg:hidden">
     <div class="flex items-center py-4">
         <!-- Navigation Toggle -->
-        <button id="toggle-button" class="text-gray-500 hover:text-gray-400 focus:outline-none focus:text-gray-400" aria-label="Toggle sidebar">
+        <button id="sidebar-toggle" class="text-gray-500 hover:text-gray-400 focus:outline-none focus:text-gray-400" aria-label="Toggle sidebar">
             <span class="sr-only">Toggle Navigation</span>
             <svg class="w-5 h-5" width="16" height="16" fill="#ffffff" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
@@ -14,10 +14,10 @@
         <ol class="ml-3 flex items-center whitespace-nowrap min-w-0" aria-label="Breadcrumb">
             <a href="/dashboard" class="flex items-center text-sm text-stone-50 dark:text-gray-400">
                 Dashboard
-                <svg class="flex-shrink-0 mx-3 overflow-visible h-2.5 w-2.5 text-gray-400 dark:text-stone-50" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-                </svg>
             </a>
+            <svg class="flex-shrink-0 mx-3 overflow-visible h-2.5 w-2.5 text-gray-400 dark:text-stone-50" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+            </svg>
             <li class="text-sm font-semibold text-stone-50 truncate" aria-current="page">
                 {{ $title }}
             </li>
