0ba7fb3b2ea0c7f387317b6e4b6352085e029db3
miftahalamsyah <miftahrizkyalamsyah@gmail.com>
2024-02-03 17:35:05 +0700

    meta description

--- resources/views/layouts/student_layout.blade.php
+++ resources/views/layouts/student_layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Gunakan Routely untuk pengalaman belajar yang baru* Dengan Routely, Anda dapat memahami routing dalam perspektif Computational Thinking, membuka peluang baru untuk pengalaman lebih pada materi routing.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="app.css">
