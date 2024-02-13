f05ac45621d2360269664fc77b664295bcaa72cb
miftahalamsyah <miftahrizkyalamsyah@gmail.com>
2024-02-13 18:38:11 +0700

    problem posing UI

--- resources/views/pages/problem-posing.blade.php
+++ resources/views/pages/problem-posing.blade.php
@@ -21,7 +21,7 @@
             </div>
 
             <div class="text-md sm:text-md md:text-md lg:text-lg">
-                <p class="p-3">Dekomposisi merupakan salah satu langkah penting dalam <em>computational thinking</em>. <em>Computational thinking</em> sendiri adalah cara berpikir yang sistematis dan terstruktur untuk menyelesaikan masalah kompleks dengan menggunakan konsep-konsep ilmu komputer. Dekomposisi berperan dalam <em>computational thinking</em> dengan membantu kita memecah masalah besar dan rumit menjadi bagian-bagian kecil yang lebih mudah dipahami dan diselesaikan secara individual.</p>
+                <p class="p-3">Menyampaikan tujuan pembelajaran dan menggali pengetahuan awal siswa adalah dua langkah penting dalam proses pembelajaran. Dengan melakukan dua langkah ini, guru dapat memastikan bahwa semua siswa dapat mencapai tujuan pembelajaran yang telah ditetapkan.</p>
             </div>
         </div>
 
@@ -40,7 +40,7 @@
             </div>
 
             <div class="text-md sm:text-md md:text-md lg:text-lg">
-                <p class="p-3">Abstraksi adalah proses mengidentifikasi dan memusatkan perhatian pada aspek-aspek penting dari suatu masalah, sambil mengabaikan detail yang tidak relevan. Dalam <em>computational thinking</em>, abstraksi membantu kita untuk:</p>
+                <p class="p-3">Pada tahap ini guru memberikan penjelasan singkat tentang materi yang akan dipelajari oleh siswa. Penjelasan ini bertujuan untuk membantu siswa memahami konsep-konsep dasar materi, membangkitkan rasa ingin tahu mereka, dan mempersiapkan mereka untuk mempelajari materi lebih lanjut.</p>
             </div>
         </div>
 
@@ -51,7 +51,7 @@
             </div>
 
             <div class="text-md sm:text-md md:text-md lg:text-lg">
-                <p class="p-3">Pengenalan pola, atau <em>pattern recognition</em>, merupakan salah satu pilar penting dalam computational thinking. Kemampuan ini memungkinkan kita untuk mengidentifikasi persamaan dan perbedaan pola dalam berbagai situasi. </p>
+                <p class="p-3">Situasi masalah dapat berupa studi kasus yang memerlukan analisis mendalam atau informasi terbuka dalam bentuk teks dan gambar. Dengan cara ini, siswa didorong untuk mengembangkan kemampuan problem posing mereka, mengonstruksi penafsiran terhadap informasi yang diberikan, dan merumuskan pertanyaan atau masalah yang relevan. </p>
             </div>
         </div>
 
@@ -70,7 +70,7 @@
             </div>
 
             <div class="text-md sm:text-md md:text-md lg:text-lg">
-                <p class="p-3">Algoritma adalah serangkaian langkah-langkah logis dan terstruktur yang digunakan untuk menyelesaikan suatu masalah</p>
+                <p class="p-3">Proses ini mendorong siswa untuk aktif terlibat dalam pembelajaran, mengeksplorasi berbagai aspek dari materi, dan mengembangkan keterampilan problem posing. Dengan berperan sebagai inisiator pertanyaan, siswa dapat memperdalam pemahaman mereka terhadap konten pembelajaran dan mengasah kemampuan kritis serta kreatif dalam menanggapi tantangan pembelajaran.</p>
             </div>
         </div>
 
@@ -81,7 +81,7 @@
             </div>
 
             <div class="text-md sm:text-md md:text-md lg:text-lg">
-                <p class="p-3">Pengenalan pola, atau <em>pattern recognition</em>, merupakan salah satu pilar penting dalam computational thinking. Kemampuan ini memungkinkan kita untuk mengidentifikasi persamaan dan perbedaan pola dalam berbagai situasi. </p>
+                <p class="p-3">Siswa berperan aktif dalam memberikan jawaban atau penyelesaian terhadap pertanyaan yang diajukan oleh siswa lainnya. Proses ini menciptakan dinamika kolaboratif di antara siswa, memungkinkan mereka saling berbagi pengetahuan dan keterampilan. Selain itu dapat memperkuat pemahaman mereka melalui penyajian konsep atau solusi. </p>
             </div>
         </div>
 
@@ -93,21 +93,45 @@
             {{-- empty div --}}
         </div>
 
-        <div id="verifikasi" class="w-full mx-auto p-5 pb-12">
+        <div id="verifikasi" class="w-full mx-auto p-5 pb-24">
             <div class="flex justify-between w-full">
                 <h2 class="clashdisplaymedium px-2 -rotate-2 text-2xl sm:text-2xl md:text-2xl lg:text-4xl bg-lime-300">06 - Verifikasi</h2>
                 <hr/>
             </div>
 
             <div class="text-md sm:text-md md:text-md lg:text-lg">
-                <p class="p-3">Algoritma adalah serangkaian langkah-langkah logis dan terstruktur yang digunakan untuk menyelesaikan suatu masalah</p>
+                <p class="p-3">Proses ini melibatkan evaluasi terhadap pemahaman konsep, kemampuan menerapkan pengetahuan, dan keterampilan yang diperoleh oleh siswa selama pembelajaran. Verifikasi bertujuan untuk memastikan bahwa siswa telah memahami dengan baik materi tersebut dan dapat mengaplikasikannya dengan tepat.</p>
             </div>
         </div>
     </div>
-
 </section>
 
-<hr class="border-2 border-orange-300">
+<section class="justify-center mx-auto px-5 bg-striped border-y-4 border-stone-800 -rotate-1">
+    <h2 class="py-12 text-center max-w-2xl mx-auto text-2xl rotate-1 clashdisplaymedium leading-none text-stone-50 sm:text-2xl lg:text-4xl">Apa Saja Klasifikasi Pembelajaran <em>Problem Posing</em> ?</h2>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 rotate-1 pb-24 max-w-5xl mx-auto">
+        <div class="block bg-lime-300 p-4 rounded-lg hover:shadow-lg border border-r-4 border-b-4 border-stone-700">
+            <h2 class="clashdisplaymedium text-lg"><em>Free Problem Posing</em></h2>
+            <p class="sm:mt-1 text-xs sm:text-xs md:text-sm sm:text-stone-700">
+                (Pengajuan masalah terbuka). Siswa diminta membuat soal dari situasi atau informasi terbuka, memungkinkan mereka mengembangkan kreativitas dan pemahaman terhadap kehidupan sehari-hari.
+            </p>
+        </div>
+        <div class="block bg-lime-300 p-4 rounded-lg hover:shadow-lg border border-r-4 border-b-4 border-stone-700">
+            <h2 class="clashdisplaymedium text-lg"><em>Semi-structured Problem Posing</em></h2>
+            <p class="sm:mt-1 text-xs sm:text-xs md:text-sm sm:text-stone-700">
+                (Pengajuan masalah semi terstruktur). Siswa diminta untuk mengembangkannya dengan menambahkan informasi atau perspektif baru, menggunakan deskripsi berbentuk pengajuan soal sebagai alat pemahaman.
+            </p>
+        </div>
+        <div class="block bg-lime-300 p-4 rounded-lg hover:shadow-lg border border-r-4 border-b-4 border-stone-700">
+            <h2 class="clashdisplaymedium text-lg"><em>Structured Problem Posing</em></h2>
+            <p class="sm:mt-1 text-xs sm:text-xs md:text-sm sm:text-stone-700">
+                (Pengajuan masalah terstruktur). Siswa merumuskan ulang soal yang telah diselesaikan atau memodifikasi dan memvariasikan pertanyaan dari masalah yang telah diberikan.
+            </p>
+        </div>
+    </div>
+</section>
+
+<hr class="py-12 bg-stone-50 border-b-4 border-lime-300"/>
+
 
 <script>
     function toggleContent(sectionId) {
